a5da0bc7ec368eee46911c78e5013465c615a38c
rockmanzb <38797460+rockmanzb@users.noreply.github.com>
2022-09-26 16:23:48 -0500

    Update detect.py
    bozhang

--- detect.py
+++ detect.py
@@ -149,8 +149,8 @@ def run(
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
-                LOGGER.info(f'bozhang debug internal')
-                print_args(vars(det))
+                LOGGER.info(f'bozhang debug internal {det}')
+                
 
                 # Print results
                 for c in det[:, -1].unique():
