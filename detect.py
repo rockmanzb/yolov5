43bad4107b15718c2def7a8c64b39c2ea41e7052
rockmanzb <38797460+rockmanzb@users.noreply.github.com>
2022-09-26 23:55:48 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -162,9 +162,11 @@ def run(
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
-                    #if save_txt:  # Write to file
-                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                    print(xywh, "bozhang coordinate", gn)
+                    c1, c2 = (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3]))
+                    center_point = round((c1[0]+c2[0])/2), round((c1[1]+c2[2])/2)
+                    print("bozhang center_point", center_point)
+                    if save_txt:  # Write to file
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         #line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
                         #with open(f'{txt_path}.txt', 'a') as f:
                             #f.write(('%g ' * len(line)).rstrip() % line + '\n')
