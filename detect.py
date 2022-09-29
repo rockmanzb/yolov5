42bbcb4d46313d9ff48aba2995abfda5560e7c56
rockmanzb <38797460+rockmanzb@users.noreply.github.com>
2022-09-28 21:28:06 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -254,7 +254,7 @@ def parse_opt():
     return opt
 
 
-def main(opt, image=1280, weights_input=yolov5l6_trained_600images.pt, src=08fd33_6.mp4, prj=DFL):
+def main(opt, image="1280", weights_input="yolov5l6_trained_600images.pt", src="08fd33_6.mp4", prj="DFL"):
     check_requirements(exclude=('tensorboard', 'thop'))
     run(**vars(opt), imgsz=image, weights=weights_input, source=src, project=prj)
 
