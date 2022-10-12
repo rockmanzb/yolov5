1664b74f1058adbb103d4dd9d3724bc265d8365a
rockmanzb <38797460+rockmanzb@users.noreply.github.com>
2022-10-12 06:31:41 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -188,7 +188,7 @@ def run(
                         x = center_point
                         start = 0
                     else:
-                        if (abs(center_point[0] - center_point_final[0]) > 224 or abs(center_point[1] - center_point_final[1]) > 224):
+                        if (abs(center_point[0] - center_point_final[0]) > 112 or abs(center_point[1] - center_point_final[1]) > 112):
                             if frame_cnt < 3:
                                 mes = np.reshape(pos[0,:],(2,1))
                                 x = kalman.correct(mes)
