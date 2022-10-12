47e4ce198fbd295349f446fba43a18afc3267d6e
rockmanzb <38797460+rockmanzb@users.noreply.github.com>
2022-10-12 04:24:06 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -179,7 +179,7 @@ def run(
                 # Write results
                 for *xyxy, conf, cls in det:
                     c1, c2 = (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3]))
-                    center_point = [round((c1[0]+c2[0])/2), round((c1[1]+c2[1])/2)]
+                    center_point = round((c1[0]+c2[0])/2), round((c1[1]+c2[1])/2)
                     pos = np.array([center_point], np.float32)
                     print (pos)
                     print("bozhang center_point", center_point, gn, xyxy)
